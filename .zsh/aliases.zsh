603feb7949f2f9eb4253c0c236a1f2e2f01915f8
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2016-02-21 15:47:54 +0200

    updates aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,9 +1,6 @@
 alias gg='echo "No project set"'
 
-alias vim='nvim'
-alias vi='nvim'
-
-# Directories
+# directories
 alias ls="ls -FG"
 alias l="ls -lhG"
 alias ll="clear && ls -lhG"
@@ -11,16 +8,18 @@ alias la='ls -AG'
 alias ..='cd ..'
 alias md='mkdir -p'
 
-# Git
+# git
 alias br='git checkout -b'
 alias ga='git add --all'
 alias gs='git status -sb'
 alias gca='git commit -am'
 alias gp='git push'
 alias gpf='git push -f'
+alias gpf='git push -f'
 
-# Apps
-alias py='python3'
+# apps
 alias tr='tree -d --prune -L 3'
 alias wl='wunderline'
-alias pg='postgres'
+alias vim='nvim'
+alias vi='nvim'
+alias gh='go-github-home'
